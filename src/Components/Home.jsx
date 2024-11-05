f11df09a0870738afea39038915f8f35d03a7c5e
Chetan Gaurkhede <gaurkhede732@gmail.com>
2024-11-06 00:35:27 +0530

    Second Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,58 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { addToPastes, updateToPastes } from "../redux/PastSlice";
 
 const Home = () => {
   const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pastId = searchParams.get("pasteId");
+  const dispatch = useDispatch();
 
+  function createPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pastId || Date.now().toString(36),
+      createdAt: new Date().toISOString(),
+    };
+
+    if (pastId) {
+      //update
+      dispatch(updateToPastes(paste))
+    } else {
+      //create
+      dispatch(addToPastes(paste))
+    }
+    //clearing all
+    setTitle('')
+    setValue('')
+    searchParams({})
+  }
   return (
-    <div>
-      <input
-        className="px-3 py-1 rounded-md mt-3"
-        type="text"
-        placeholder="Enter Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button>
-        Enter Paste
-      </button>
+    <div className="min-w-[800px]">
+      <div className="flex flex-row justify-between w-full">
+        <input
+          className="px-5 py-3 rounded-md mt-3 w-[50%]"
+          type="text"
+          placeholder="Enter Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button onClick={createPaste} className="ml-3">
+          {pastId ? "Update My Paste" : "Create New Paste"}
+        </button>
+      </div>
+      <div>
+        <textarea
+          className="w-full p-2 rounded-md mt-5"
+          value={value}
+          placeholder="Enter your content"
+          onChange={(e) => setValue(e.target.value)}
+          rows={20}
+        />
+      </div>
     </div>
   );
 };
--- src/redux/PastSlice.js
+++ src/redux/PastSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import toast from 'react-hot-toast';
 
 const initialState = {
   pastes: localStorage.getItem("pastes") ? JSON.parse(localStorage.getItem("pastes")) : []
@@ -9,7 +10,10 @@ export const pasteSlice = createSlice({
   initialState,
   reducers: {
     addToPastes: (state, action) => {
-
+      const paste = action.payload;
+      state.pastes.push(paste);
+      localStorage.setItem("pastes", JSON.stringify(state.pastes));
+      toast("Paste Create Successfully");
     },
     updateToPastes: (state, action) => {
 
@@ -24,6 +28,6 @@ export const pasteSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes} = pasteSlice.actions
+export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes } = pasteSlice.actions
 
 export default pasteSlice.reducer
